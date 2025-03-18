062341511e20a3e0162335b02024194c9682d282
wjwj910 <wjwj910@gmail.com>
2025-03-18 09:59:33 +0900

    work

--- src/test/kotlin/com/back/Kp20250317ApplicationTests.kt
+++ src/test/kotlin/com/back/Kp20250317ApplicationTests.kt
@@ -2,7 +2,9 @@ package com.back
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Kp20250317ApplicationTests {
 
